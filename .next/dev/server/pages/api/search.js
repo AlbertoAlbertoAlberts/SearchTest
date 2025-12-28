878df1617ca4d169f0c13edbc34edb7e5b7abae4
MTyyk <mtkxtc@gmail.com>
2025-12-28 18:57:08 +0200

    Stop tracking .next build artifacts

--- .next/dev/server/pages/api/search.js
+++
@@ -1,5 +0,0 @@
-var R=require("../../chunks/[turbopack]_runtime.js")("server/pages/api/search.js")
-R.c("server/chunks/[root-of-the-server]__fde5b660._.js")
-R.c("server/chunks/[root-of-the-server]__2192f4dd._.js")
-R.m("[project]/node_modules/next/dist/esm/build/templates/pages-api.js { INNER_PAGE => \"[project]/pages/api/search.js [api] (ecmascript)\" } [api] (ecmascript)")
-module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/pages-api.js { INNER_PAGE => \"[project]/pages/api/search.js [api] (ecmascript)\" } [api] (ecmascript)").exports
